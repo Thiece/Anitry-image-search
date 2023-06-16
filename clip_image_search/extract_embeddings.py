eebbc1bbfd302e972c7ad02930a30515c49c0712
Pluto <thiece@live.com>
2023-06-16 17:32:59 +0800

    Update extract_embeddings.py
    修改openai clip为OFA-Sys/Chinese-CLIP

--- clip_image_search/extract_embeddings.py
+++ clip_image_search/extract_embeddings.py
@@ -1,4 +1,4 @@
-import clip
+import cn_clip.clip as clip
 from search_dataset import ClipSearchDataset
 import os
 from torch.utils.data import DataLoader
@@ -10,14 +10,14 @@ import click
 @click.command()
 @click.option('--img_dir', default='/home/data/lumen', help='Directory of images.')
 @click.option('--save_path', default='results/embeddings.pkl', help='Path to save the embeddings.')
-@click.option('--batch_size', default=256, help='Batch size for DataLoader.')
-@click.option('--num_workers', default=40, help='Number of workers for DataLoader.')
+@click.option('--batch_size', default=64, help='Batch size for DataLoader.')
+@click.option('--num_workers', default=8, help='Number of workers for DataLoader.')
 def compute_embeddings(img_dir, save_path, batch_size, num_workers):
     if not os.path.exists(os.path.dirname(save_path)):
         os.makedirs(os.path.dirname(save_path))
 
     device = "cuda" if torch.cuda.is_available() else "cpu"
-    model, preprocess = clip.load('ViT-B/32', device)
+    model, preprocess = clip.load_from_name('ViT-H-14', device=device, download_root='./chinese_clip')
     dataset = ClipSearchDataset(img_dir = img_dir, preprocess = preprocess)
     dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=False, num_workers=num_workers)
 
